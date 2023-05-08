743970e44c806b3e46a56ca349628992ec190d90
vladislav.Baiborodov <baiborodov333@mail.ru>
2023-05-08 17:47:13 +0500

    finish-pravka

--- js-features/mole-game/task.js
+++ js-features/mole-game/task.js
@@ -2,7 +2,6 @@ const hole = document.querySelectorAll('.hole')
 const dead = document.getElementById('dead')
 const lost = document.getElementById('lost')
 
-
 let deadGame = 0;
 let lostGame = 0;
 
@@ -16,12 +15,19 @@ hole.forEach(holes => {
 			lostGame++
 			lost.innerHTML = lostGame
 		}
-		if (deadGame > 10) {
-			deadGame = 0;
-			alert('nice victory!');
-		} else if (lostGame > 5) {
-			lostGame = 0;
-			alert('Game Over!');
+		if (deadGame > 9) {
+			alert('you win')
+			resetGame()
+		} else if (lostGame > 4) {
+			alert('game over')
+			resetGame()
 		}
 	})
 })
+
+function resetGame() {
+	deadGame = 0;
+	lostGame = 0;
+	dead.innerHTML = deadGame;
+	lost.innerHTML = lostGame;
+}
